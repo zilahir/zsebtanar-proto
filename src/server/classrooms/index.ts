490466eec748143b5536a2205dbfd439c54327d2
Richard Zilahi <richard.zilahi@salesframe.com>
2020-04-04 23:59:00 +0300

    creating classrooms

--- src/server/classrooms/index.ts
+++ src/server/classrooms/index.ts
@@ -4,16 +4,15 @@ import { errorHandler } from '../utils/error'
 import { admin } from '../utils/firebase'
 export const route = express.Router()
 const DB = admin.database()
-const ClassRooms = DB.ref('classrooms/')
+const ClassRooms = DB.ref('classrooms')
 
 route.post('/insert',
 	(req, res) => {
 		const body = req.body
-		ClassRooms.set({
-			1: {
-				name: body.name,
-				ownerId: body.ownerId,
-			}
+		const key = DB.ref('classrooms').push().key
+		ClassRooms.child(key).set({
+			name: body.name,
+			ownerId: body.ownerId,
 		})
 		res.status(200).send({
 			success: true
