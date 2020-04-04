5539a2cf3451a882f22478bdf3edc5af16bf075f
Richard Zilahi <richard.zilahi@salesframe.com>
2020-04-04 13:07:07 +0300

    implemenitng teacher page

--- src/client-public/Routes.tsx
+++ src/client-public/Routes.tsx
@@ -20,6 +20,7 @@ import { Profile } from './page/Profile'
 import { Search } from './page/Search'
 import { Workarounds } from '../client-common/page/Workarounds'
 import { CookieConsent } from './component/CookieConsent'
+import TeacherPage from './page/Teacher'
 
 interface RoutersProps {
   history: History
@@ -68,6 +69,7 @@ const App = props => () => {
           {props.session.signedIn && <Route path="/profile" component={Profile} />}
           <Route path="/subject/:subject/:topic" component={ExercisesByTopic} />
           <Route path="/grade/:grade" component={ExercisesByGrade} />
+          <Route path="/teacher" component={TeacherPage} />
           <Route path="/search" component={Search} />
           <Route path="/about" component={About} />
           <Route path="/joinus" component={JoinUs} />
---
+++ src/client-public/page/Teacher/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+import './Teacher.module.scss'
+
+const TeacherPage = () => (
+    <div className="row">
+        <div className="col-lg-12">
+            <div className="teacherWrapper">
+                <h4>
+                    Új osztály létrehozása
+                </h4>
+            </div>
+        </div>
+    </div>
+)
+
+export default TeacherPage
