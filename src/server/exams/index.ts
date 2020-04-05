5f96924f74142a72615d9d5ad8d80ae48c2f1b1f
Richard Zilahi <richard.zilahi@salesframe.com>
2020-04-05 18:06:49 +0300

    added apiendpoint for getting exams for user

--- src/server/exams/index.ts
+++ src/server/exams/index.ts
@@ -23,3 +23,21 @@ route.post('/insert',
 		})
 	}
 )
+
+route.get('/getall',
+	(req, res) => {
+		const body = req.body
+		let result = []
+		Exams.on('value', (snap) => {
+			snap.forEach(item => {
+				const currVal = item.val()
+				currVal.ownerId === body.ownerId
+				result.push(currVal)
+			})
+		})
+		res.status(200).send({
+			success: true,
+			classRooms: [...result]
+		})
+	}
+)
