cc1eba013e20b2aef632480a03cefd43e139c490
Richard Zilahi <richard.zilahi@salesframe.com>
2020-04-05 12:02:42 +0300

    getting all classrooms for user

--- src/server/classrooms/index.ts
+++ src/server/classrooms/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express'
 import { errorHandler } from '../utils/error'
 
 import { admin } from '../utils/firebase'
+import curry from 'ramda/es/curry'
 export const route = express.Router()
 const DB = admin.database()
 const ClassRooms = DB.ref('classrooms')
@@ -16,6 +17,24 @@ route.post('/insert',
 		})
 		res.status(200).send({
 			success: true
+		})
+	}
+)
+
+route.get('/getall',
+	(req, res) => {
+		const body = req.body
+		let result = []
+		ClassRooms.on('value', (snap) => {
+			snap.forEach(item => {
+				const currVal = item.val()
+				currVal.ownerId === body.ownerId
+				result.push(currVal)
+			})
+		})
+		res.status(200).send({
+			success: true,
+			classRooms: [...result]
 		})
 	}
 )
