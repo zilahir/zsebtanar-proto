63cdc7fe716f744ee47ebb3b0c4b9b949276805d
Richard Zilahi <richard.zilahi@salesframe.com>
2020-04-05 18:01:37 +0300

    implemenitng reducer for getting all exams

--- src/client-common/services/exam.ts
+++ src/client-common/services/exam.ts
@@ -1,4 +1,11 @@
-import { cloudFnPost } from '../util/firebase'
+import { cloudFnPost, cloudFnGet } from '../util/firebase'
+
 export const createNewExam = newExamObject => (
 	cloudFnPost(`exam/insert`, newExamObject, { withToken: true }).then()
+)
+
+export const getAllExam = ownerId => (
+	cloudFnGet(`exam/getall`, { ownerId }, { withToken: true }).then(result => {
+		return result.data
+	})
 )
---
+++ src/client-common/store/reducers/exams.ts
@@ -0,0 +1,19 @@
+import { GET_ALL_EXAMS } from '../types'
+
+const initialState = {
+	classrooms: [],
+}
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+	case GET_ALL_EXAMS:
+		return {
+			...state,
+			exams: action.payload,
+		}
+	default:
+		return state
+	}
+}
+
+export default reducer
